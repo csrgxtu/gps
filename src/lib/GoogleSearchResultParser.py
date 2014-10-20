7b6befd2a648f798c0a0d0f65565722c9c491f7b
csrgxtu <archerreilly@gmail.com>
2014-10-19 21:24:02 -0400

    GoogleSearchResultParser ready

---
+++ src/lib/GoogleSearchResultParser.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding = utf8
+# Author: Archer Reilly
+# Date: 19/Oct/2014
+# File: GoogleSearchResultParser.py
+# Desc: this class is used to parse the Google search result,
+# get the json representation of the search result.
+# {
+#   'results': [
+#     {
+#       'title': 'Beautiful Soup: We called him ..',
+#       'unescapedUrl': '',
+#       'escapedUrl': '',
+#       'content': ''
+#     },
+#     {
+#       'title': 'Beautiful Soup: We called him ..',
+#       'unescapedUrl': '',
+#       'escapedUrl': '',
+#       'content': ''
+#     },
+#     ...
+#   ]
+# }
+#
+# Produced By CSRGXTU
+from bs4 import BeautifulSoup
+#from json import dumps
+
+class GoogleSearchResultParser(object):
+  # hold the original html source code
+  html = None
+
+  # soup object
+  soup = None
+
+  # result sets
+  resultSets = None
+
+  # json data
+  jsonData = {}
+
+  def __init__(self, html):
+    self.html = html
+    self.soup = BeautifulSoup(self.html)
+    self.resultSets = self.soup.find_all("li", class_="g")
+  
+  """
+  def getResultSets(self):
+    self.resultSets = self.soup.find_all("li", class_="g")
+  """
+
+  def getTitle(self, item):
+    return item.find("h3", class_="r").find("a").get_text()
+
+  def getUnescapedUrl(self, item):
+    return item.find("h3", class_="r").find("a")["href"]
+
+  def getEscapedUrl(self, item):
+    return item.find("div", class_="s").find("cite").get_text()
+
+  def getContent(self, item):
+    return item.find("div", class_="s").find("span", class_="st").get_text()
+  
+  def getResultStats(self):
+    return unicode(self.soup.select("#resultStats")[0].get_text()).encode('utf8')
+
+  def getRelatedKeyWords(self):
+    tmpDict = {}
+    tags = self.soup.find_all("p", class_="_Bmc")
+    i = 0
+    for item in tags:
+      tmpDict[i] = unicode(item.find("a").get_text()).encode('utf8')
+      i = i + 1
+    return tmpDict
+
+  def getJson(self):
+    tmpLst = []
+    for item in self.resultSets:
+      title = unicode(self.getTitle(item)).encode('utf8')
+      unescapedUrl = unicode(self.getUnescapedUrl(item)).encode('utf8')
+      escapedUrl = unicode(self.getEscapedUrl(item)).encode('utf8')
+      content = unicode(self.getContent(item)).encode('utf8')
+      tmpDict = {'title': title, 'unescapedUrl': unescapedUrl, 'escapedUrl': escapedUrl, 'content': content}
+      tmpLst.append(tmpDict)
+    self.jsonData['results'] = tmpLst
+    self.jsonData['resultStats'] = self.getResultStats()
+    self.jsonData['relatedKeyWords'] = self.getRelatedKeyWords()
+    return self.jsonData
